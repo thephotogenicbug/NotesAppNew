34ead7b827c1ac3f5e89d153c8a18750386c5a55
thephotogenicbug <navinkumarachari@gmail.com>
2022-08-29 19:15:39 +0530

    KotlinNotesAPP(Display Splashscreen on app load and hide toolbar)

--- app/src/main/java/com/naveen/notesapp/SplashScreen.kt
+++ app/src/main/java/com/naveen/notesapp/SplashScreen.kt
@@ -1,11 +1,23 @@
 package com.naveen.notesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // Hide Toolbar in splashScreen
+        supportActionBar?.hide()
+
+        // Display SplashScreen For 2Sec
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            //code
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 2000)
     }
 }
